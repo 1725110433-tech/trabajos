ab57996b9dcef3f2ff4aaad52234b5b4c43491e4
1725110433-tech <1725110433@utectulancingo.edu.mx>
2026-05-21 04:44:05 +0000

    CREATED banco.py

--- banco.py
+++ banco.py
@@ -1,7 +1,7 @@
 class banco:
-    def __init__(self, no_clientes, no_guardias, no_edificios, sistema_informatico,
+    def __init__(self, no_cliente, no_guardias, no_edificios, sistema_informatico,
                 nombre_banco, no_cajeros, fiable, capital, horario_atencion, color_banco):
-        self. no_clientes = no_clientes
+        self. no_cliente = no_cliente
         self. no_guardias = no_guardias
         self. no_edificios = no_edificios
         self. sistema_informatico = sistema_informatico
@@ -11,8 +11,8 @@ class banco:
         self. capital = capital
         self. horario_atencion = horario_atencion
         self. color_banco = color_banco
-        print("f No. Clientes {self.no_clientes}")
-        print("f No. Guardias {self.no_guardias}")  
+        print("f no clientes {self.no_cliente}")
+        print("f no guardias {self.no_guardias}")  
         print("f No. Edificios {self.no_edificios}")
         print("f Sistema Informatico {self.sistema_informatico}")
         print("f Nombre Banco {self.nombre_banco}") 
@@ -21,4 +21,27 @@ class banco:
         print("f Capital {self.capital}")
         print("f Horario Atencion {self.horario_atencion}")
         print("f Color Banco {self.color_banco}")
+
+    def retiro(self): 
+        print(f"{self.no_cliente} hizo un retiro ")
+
+    def deposito(self): 
+        print(f"{self.no_cliente} hizo deposito") 
+
+    def cheque(self): 
+        print(f"{self.no_cliente} le dieron cheque")
+
+    def pago(self): 
+        print(f"{self.no_cliente} hizo pago ")
+
+    def inversion(self): 
+        print(f"{self.no_cliente} hizo inversion ")
+
+
 acme=banco("1000 clientes", "none", "none", "ACME Banking System", "Banco Acme", "1000", "True", "$1,000,000,000", "9am-19pm", "Verde Fosforecente")
+
+acme.retiro()
+acme.deposito()
+acme.cheque()
+acme.pago()
+acme.inversion()
